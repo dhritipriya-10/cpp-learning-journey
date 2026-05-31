125474fce7fdf4d62c4768e412af9424baeca415
Dhriti Priya <dhritipriya10@gmail.com>
2026-05-31 23:36:07 +0530

    Day 6: Arrays - Count Even Numbers

---
+++ Arrays/Day 6/array6_count_even.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+void ary() {
+    int count = 0;
+    int arr[5];
+    for(int i = 0; i < 5; i++) {
+        cin >> arr[i];
+        if(arr[i] % 2 == 0) {
+            count++;
+        }
+    }
+    cout << count;
+}
+int main() {
+    cout<< "Enter the values: ";
+    ary();
+    return 0;
+}
