4459e3475039890688afcebf20cb243a534cd348
Dhriti Priya <dhritipriya10@gmail.com>
2026-06-07 22:56:45 +0530

    Day 12: Count elements greater than X

---
+++ Arrays/Day 12/array15_count_greater_than_x.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+void countGreaterX() {
+    int n;
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements in the array: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int X;
+    cout << "Enter the value of X: ";
+    cin >> X;
+    int count = 0;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] > X) {
+            count++;
+        }
+    }
+    cout << "Count = " << count << endl;
+}
+int main() {
+    cout << "Enter the value of n: ";
+    countGreaterX();
+    return 0;
+}
