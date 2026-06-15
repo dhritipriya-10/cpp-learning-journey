35b297f97d8914cec78bf4ad0d74d80cb7a5cef4
Dhriti Priya <dhritipriya10@gmail.com>
2026-06-16 00:27:40 +0530

    Day 21: Reversing the elements in an array

---
+++ Arrays/Day 21/array27_reverse_array.cpp
@@ -0,0 +1,22 @@
+//Reverse the array elements.
+
+#include<bits/stdc++.h>
+using namespace std;
+void revarr() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    cout<<"Enter the elements in the array: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    for(int i = n-1; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int main() {
+    cout << "Enter the value of n: ";
+    revarr();
+    return 0;
+}
